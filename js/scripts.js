51d819dfded6af5a03d44695cb7eed03504d3a4c
Steve Hickey <steve@stevehickeydesign.com>
2013-07-14 15:43:49 -0400

    Get gift list styles in order, fix some other small problems

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,22 @@
   "use strict";
 
   $('#create-an-account').submit( function() {
-    var email = $('#email').val();
-    var password = $('#password').val();
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-    window.location = 'create-profile.html';
+    var currentUserEmail = $('#current-user-email').val();
+    var currentUserPassword = $('#current-user-password').val();
+    localStorage.setItem('currentUserEmail', currentUserEmail);
+    localStorage.setItem('currentUserPassword', currentUserPassword);
+    window.location = '/create-profile';
+    return false;
+  });
+
+  $('#create-your-profile').submit( function() {
+    var currentUserName = $('#current-user-name').val();
+    var currentUserLocation = $('#current-user-location').val();
+    var currentUserAge = $('#current-user-age').val();
+    localStorage.setItem('currentUserName', currentUserName);
+    localStorage.setItem('currentUserLocation', currentUserLocation);
+    localStorage.setItem('currentUserAge', currentUserAge);
+    window.location = '/your-list';
     return false;
   });
 
