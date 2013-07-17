4854e6466f6c4f2525c9ceb3b1bc25cec08f2efd
Steve Hickey <steve@stevehickeydesign.com>
2013-07-16 20:52:36 -0400

    Refine existing forms and create log in form

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
 (function () {
   "use strict";
 
-  $('#create-an-account').submit( function() {
-    var currentUserEmail = $('#current-user-email').val();
-    var currentUserPassword = $('#current-user-password').val();
-    localStorage.setItem('currentUserEmail', currentUserEmail);
-    localStorage.setItem('currentUserPassword', currentUserPassword);
+  $('#log-in-link').click( function() {
+    $(this).addClass('hide');
+    $('#log-in-form').addClass('show');
+    $('#log-in-email').focus();
+    return false;
+  });
+
+  $('#log-in-form').submit( function() {
+    var logInEmail = $('#log-in-email').val();
+    var logInPassword = $('#log-in-password').val();
+    localStorage.setItem('currentUserEmail', logInEmail);
+    localStorage.setItem('currentUserPassword', logInPassword); // I know this is wrong. I'll fix it when I create a "real" log in system
+    window.location = '/your-list';
+    return false;
+  });
+
+  $('#create-an-account-form').submit( function() {
+    var createAnAccountEmail = $('#create-an-account-email').val();
+    var createAnAccountPassword = $('#create-an-account-password').val();
+    localStorage.setItem('currentUserEmail', createAnAccountEmail);
+    localStorage.setItem('currentUserPassword', createAnAccountPassword);
     window.location = '/create-profile';
     return false;
   });
 
-  $('#create-your-profile').submit( function() {
-    var currentUserName = $('#current-user-name').val();
-    var currentUserLocation = $('#current-user-location').val();
-    var currentUserAge = $('#current-user-age').val();
-    localStorage.setItem('currentUserName', currentUserName);
-    localStorage.setItem('currentUserLocation', currentUserLocation);
-    localStorage.setItem('currentUserAge', currentUserAge);
+  $('#create-your-profile-form').submit( function() {
+    var createYourProfileName = $('#create-your-profile-name').val();
+    var createYourProfileLocation = $('#create-your-profile-location').val();
+    var createYourProfileAge = $('#create-your-profile-age').val();
+    localStorage.setItem('currentUserName', createYourProfileName);
+    localStorage.setItem('currentUserLocation', createYourProfileLocation);
+    localStorage.setItem('currentUserAge', createYourProfileAge);
     window.location = '/your-list';
     return false;
   });
